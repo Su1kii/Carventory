f95aa34e2872fd239f02860066349843b79f4859
Steven Echeverria <stevennava749@gmail.com>
2025-06-03 19:52:08 -0600

    Update CarCard.tsx
    Mobile Friendly Ui CarCard

--- src/app/cars/[slug]/CarCard.tsx
+++ src/app/cars/[slug]/CarCard.tsx
@@ -7,7 +7,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Badge } from "lucide-react";
 
 type Car = Awaited<ReturnType<typeof getCarById>>;
 
@@ -21,30 +20,36 @@ export default function CarCard({ car }: CarCardProps) {
   }
 
   return (
-    <Card className="max-w">
-      <div className="flex flex-row">
-        <div className="basis-2/4">
+    <Card className="max-w-full mx-auto">
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-1/2">
           <CardHeader>
             {car.imageUrl && (
               <div className="rounded-lg overflow-hidden">
                 <img
                   src={car.imageUrl}
-                  alt="Post content"
+                  alt="Car"
                   className="w-full h-auto object-cover"
                 />
               </div>
             )}
           </CardHeader>
         </div>
-        <div className="basis-2/4 flex flex-col justify-between">
-          <CardContent className="mt-8 space-y-3">
-            <CardTitle className="text-5xl font-bold">
+        <div className="md:w-1/2 flex flex-col justify-between">
+          <CardContent className="mt-4 md:mt-8 space-y-3">
+            <CardTitle className="text-3xl md:text-5xl font-bold">
               {car.make} {car.model}
             </CardTitle>
-            <CardTitle className="text-3xl font-bold">${car.price}</CardTitle>
-            <CardDescription>{car.carType}</CardDescription>
-            <CardDescription>Year: {car.year}</CardDescription>
-            <CardDescription className="font-bold">
+            <CardTitle className="text-2xl md:text-3xl font-bold">
+              ${car.price}
+            </CardTitle>
+            <CardDescription className="text-sm md:text-base">
+              {car.carType}
+            </CardDescription>
+            <CardDescription className="text-sm md:text-base">
+              Year: {car.year}
+            </CardDescription>
+            <CardDescription className="font-semibold text-sm md:text-base">
               {car.description}
             </CardDescription>
           </CardContent>
